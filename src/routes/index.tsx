7f5510ea20ad4d9a5c403f9e7dcc0a1209f2313f
Leonard Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2025-10-02 11:36:01 +0300

    hero

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,13 @@ import HeroSection1 from '@/components/HeroSection1'
 import HeroSection2 from '@/components/HeroSection2'
 import Features from '@/components/Features'
 import TestimonialsSection from '@/components/TestimonialsSection'
+import Hero from '@/components/Hero'
 
 export const Route = createFileRoute('/')({
     head: () => ({
         meta: [
             {
-                title: 'LinkHub | Home',
+                title: 'Home - LinkHub',
             },
         ],
     }),
@@ -25,6 +26,14 @@ function RouteComponent() {
         <Container>
             {/* Top navigation */}
             <Menu context={{ user: routeContext.user, userProfile: routeContext.userProfile }} />
+
+            <section className='mt-16 py-12'>
+                <div className="relative border-2 border-white/70 bg-white/40 shadow-sm ring-1 ring-white/50 backdrop-blur">
+                    <div className="aspect-[16/9] w-full">
+                        <Hero />
+                    </div>
+                </div>
+            </section>
 
             {/* Hero section 1 */}
             <HeroSection1 />
